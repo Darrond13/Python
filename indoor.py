81d56f172da075796b53def361b9c2ee9e9db657
Darron C <183058256+Darrond13@users.noreply.github.com>
2025-03-13 16:02:45 -0500

    Indoor

---
+++ indoor.py
@@ -0,0 +1,4 @@
+#write a program that asks the user for input and repeats it back in lower case "indoor voice"
+
+text = input("I will copy what you type out in caps in lower case..... ")
+print(text.lower())
